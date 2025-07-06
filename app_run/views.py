f43040c5a44d2faa3fe5b89a10f69f80ff35ad12
linni <linnik.aleksandr@mail.ru>
2025-07-06 21:30:00 +0700

    Merge remote-tracking branch 'origin/main'

--- app_run/views.py
+++ app_run/views.py
@@ -1,6 +1,14 @@
 from django.conf import settings
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import viewsets
+from .models import Run
+from .serializers import RunSerializer
+
+
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
 
 
 @api_view(['GET'])
@@ -11,3 +19,8 @@ def company_info(request):
         'contacts': settings.CONTACTS,
     }
     return Response(details)
+
+
+
+
+
