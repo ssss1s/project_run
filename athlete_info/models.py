587bb00875dfdf0ae3ecbeba43dede0408688fca
linni <linnik.aleksandr@mail.ru>
2025-07-12 01:32:15 +0700

    Merge remote-tracking branch 'origin/main'

--- athlete_info/models.py
+++ athlete_info/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class AthleteInfo(models.Model):
     weight = models.FloatField(null=True, blank=True)  # Разрешаем NULL
-    goals = models.CharField(max_length=50, null=True, blank=True)  # Разрешаем NULL
+    goals = models.CharField(max_length=10000, null=True, blank=True)  # Разрешаем NULL
     Info = models.OneToOneField(User, on_delete=models.CASCADE, related_name='athlete_info')
 
 
