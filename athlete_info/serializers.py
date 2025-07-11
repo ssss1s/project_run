b5cc31f3a2858df989f6cc73902f2c5a53bdd6ee
linni <linnik.aleksandr@mail.ru>
2025-07-12 01:39:40 +0700

    Merge remote-tracking branch 'origin/main'

--- athlete_info/serializers.py
+++ athlete_info/serializers.py
@@ -16,11 +16,6 @@ class AthleteInfoSerializer(serializers.ModelSerializer):
             'goals': {'required': False, 'allow_null': True}
         }
 
-    def validate_goals(self, value):
-        if value and len(value) > 50:
-            raise serializers.ValidationError("Максимальная длина цели - 50 символов")
-        return value
-
     def validate_weight(self, value):
         if value <= 0 or value >= 900:
             raise serializers.ValidationError("Вес должен быть между 0 и 900 кг.")
