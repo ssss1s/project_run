98c085322e6c1a3e11177b79c828965f66ba32c0
linni <linnik.aleksandr@mail.ru>
2025-07-25 18:33:08 +0700

    Merge remote-tracking branch 'origin/main'

--- latitudelongitude/views.py
+++ latitudelongitude/views.py
@@ -1,6 +1,5 @@
 from django.utils import timezone
 from decimal import Decimal
-
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, status
 from rest_framework.response import Response
