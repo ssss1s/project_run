3a9e59db30c364f896a798883cb000bd74fb2b2e
linni <linnik.aleksandr@mail.ru>
2025-08-01 20:38:51 +0700

    Merge remote-tracking branch 'origin/main'

--- subscribe/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import SubscribeToCoachViewAPIView
-
-urlpatterns = [
-    path('api/subscribe_to_coach/<int:id>/', SubscribeToCoachViewAPIView, name='subscribe_to_coach'),
-]
